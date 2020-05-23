84c771e637c53cf0d67f868bb9da501953e0a1d6
Nithin K V <nithin.kulangaraveedu@gmail.com>
2020-05-23 21:38:10 +0530

    Updates

---
+++ precache-manifest.f352afcb0bfb721f6f8262bb5925156b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09636ae3aac8db2dff3c47a651ce12c6",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "81c60b3770546a60310e",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "2cf30bc240915eb2bd6f",
+    "url": "/robofriends/static/css/main.21a05347.chunk.css"
+  },
+  {
+    "revision": "81c60b3770546a60310e",
+    "url": "/robofriends/static/js/2.ce3203d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.ce3203d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cf30bc240915eb2bd6f",
+    "url": "/robofriends/static/js/main.81b144af.chunk.js"
+  },
+  {
+    "revision": "99557a09ab68caec6407",
+    "url": "/robofriends/static/js/runtime-main.229ccf3b.js"
+  }
+]);
